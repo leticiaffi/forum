a9cdad282a684c75d368874e0cc4ec01c9600fcb
Leticia Israel <leticiafisrael@gmail.com>
2022-10-28 15:24:54 -0300

    salvar

--- gestao-vendas/src/main/java/com/gvendas/gestaovendas/excecao/GestaoVendasTratamentoExcecao.java
+++ gestao-vendas/src/main/java/com/gvendas/gestaovendas/excecao/GestaoVendasTratamentoExcecao.java
@@ -1,5 +1,6 @@
 package com.gvendas.gestaovendas.excecao;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -33,6 +34,13 @@ import java.util.List;
         String msgDesenvolvedor = ex.toString();
         List<Erro> erros = Arrays.asList(new Erro(msgUsuario, msgDesenvolvedor));
         return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request){
+        String msgUsuario = "Recurso não encontrado.";
+        String msgDesenvolvedor = ex.toString();
+        List<Erro> erros = Arrays.asList(new Erro(msgUsuario, msgDesenvolvedor));
+        return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
     @ExceptionHandler(RegraNegocioException.class)
     public ResponseEntity<Object> handleRegraNegocioException(RegraNegocioException ex, WebRequest request){
